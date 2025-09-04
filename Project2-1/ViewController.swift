5e241af5f6c43d42c72a60fc56d708d4d516db1d
황동혁 <ghkdehdgur01@gmail.com>
2025-09-04 11:25:12 +0900

    Initial commit

---
+++ Project2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2-1
+//
+//  Created by 황동혁 on 9/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
