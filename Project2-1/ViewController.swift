8b55ca30267c8340eeb62af18303a372b2bb2587
남병수 <nambyoungsu@gmail.com>
2025-09-05 15:15:21 +0900

    new project

---
+++ Project2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2-1
+//
+//  Created by 남병수 on 9/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
