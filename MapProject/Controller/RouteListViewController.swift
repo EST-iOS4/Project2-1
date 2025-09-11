b47771439635413e7320df73948bbc4db453253b
남병수 <nambyoungsu@gmail.com>
2025-09-11 11:46:31 +0900

    편집 모드 추가

--- MapProject/Controller/RouteListViewController.swift
+++ MapProject/Controller/RouteListViewController.swift
@@ -1,71 +1,96 @@
 import UIKit
 
 class RouteListViewController: UIViewController {
-
-    // MARK: - Properties
   
-    var places: [Place] = [
-        Place(name: "강남역", address: "서울 강남구 강남대로"),
-        Place(name: "코엑스", address: "서울 강남구 영동대로 513"),
-        Place(name: "롯데월드타워", address: "서울 송파구 올림픽로 300")
-    ]
+  // MARK: - Properties
+  
+  var places: [Place] = [
+    Place(name: "강남역", address: "서울 강남구 강남대로"),
+    Place(name: "코엑스", address: "서울 강남구 영동대로 513"),
+    Place(name: "롯데월드타워", address: "서울 송파구 올림픽로 300")
+  ]
+  
+  private let tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "placeCell")
+    return tableView
+  }()
+  
+  // MARK: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "placeCell")
-        return tableView
-    }()
-
-    // MARK: - Lifecycle
+    setupUI()
+    setupTableView()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupUI()
-        setupTableView()
-    }
+    navigationItem.rightBarButtonItem = editButtonItem
+  }
+  
+  override func setEditing(_ editing: Bool, animated: Bool) {
+      super.setEditing(editing, animated: animated)
+      
+      tableView.setEditing(editing, animated: animated)
+  }
+  
+  // MARK: - Setup
+  
+  private func setupUI() {
+    view.backgroundColor = .systemBackground
+    title = "경로 설정"
     
-    // MARK: - Setup
+    view.addSubview(tableView)
     
-    private func setupUI() {
-        view.backgroundColor = .systemBackground
-        title = "경로 설정"
-        
-        view.addSubview(tableView)
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-    }
-    
-    private func setupTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+    ])
+  }
+  
+  private func setupTableView() {
+    tableView.dataSource = self
+    tableView.delegate = self
+  }
 }
 
 // MARK: - UITableView DataSource & Delegate
 
 extension RouteListViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return places.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.count
-    }
+    let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell", for: indexPath)
+    let place = places[indexPath.row]
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell", for: indexPath)
-        
-        let place = places[indexPath.row]
-        
-        var content = cell.defaultContentConfiguration()
-        content.text = place.name
-        content.secondaryText = place.address
-        cell.contentConfiguration = content
-        
-        return cell
+    var content = cell.defaultContentConfiguration()
+    content.text = place.name
+    content.secondaryText = place.address
+    cell.contentConfiguration = content
+    
+    return cell
+    
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      places.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
     }
+  }
+  
+  func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    let movedPlace = places.remove(at: sourceIndexPath.row)
+    places.insert(movedPlace, at: destinationIndexPath.row)
+  }
 }
